cc4c06b5a87bff91382ce1f88cc6164f1cff0f79
Sonke Gumede <129692205+sonkeGumede@users.noreply.github.com>
2024-05-13 17:05:34 +0200

    featr: update inputs

---
+++ src/components/Inputs/index.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  && {
+    height: 30px;
+    border: solid thin ${({ theme }) => theme.colors.cloudyBlue};
+    color: ${({ theme }) => theme.colors.charcoalGrey};
+    border-radius: 5px;
+    width: 80%;
+    padding: 0px 5px;
+    &:focus {
+      outline: none !important;
+      border: 1px solid ${({ theme }) => theme.colors.yellow};
+    }
+  }
+`;
+
+const Input: React.FunctionComponent<React.InputHTMLAttributes<any>> = (props) => {
+  return <StyledInput {...props} />;
+};
+
+export default Input;
