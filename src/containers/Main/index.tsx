abf4417bb38544087006c9174509f9ecf0c0ba82
Sonke Gumede <129692205+sonkeGumede@users.noreply.github.com>
2024-05-13 17:03:32 +0200

    improve: update main component

--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Left from "../Left";
 import Right from "../Right";
+import { useState } from "react";
 const Main = styled.div`
   margin-top: 30px;
   border-radius: 20px;
@@ -12,11 +13,15 @@ const Main = styled.div`
   margin-right: auto;
 `;
 
-const Container:React.FunctionComponent<{}> = () => {
+const Container: React.FunctionComponent<{}> = () => {
+  const [toggled, setToggle] = useState<boolean>(false);
+  const onSetToggle = () => {
+    setToggle(!toggled);
+  };
   return (
     <Main>
-      <Left />
-      <Right />
+      <Left onToggle={onSetToggle} toggled={toggled} />
+      <Right toggled={toggled} />
     </Main>
   );
 };
