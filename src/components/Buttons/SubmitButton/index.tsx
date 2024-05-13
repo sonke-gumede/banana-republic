efaed6444355dd6a2660a03a125acadb88351d2e
Sonke Gumede <129692205+sonkeGumede@users.noreply.github.com>
2024-05-13 17:04:42 +0200

    feat: submit buttons

---
+++ src/components/Buttons/SubmitButton/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button<{ varient: "default" | "primary" }>`
+  && {
+    color: ${({ theme }) => theme.colors.charcoalGrey};
+    width: 100px;
+    height: 40px;
+    background-color: ${({ theme, varient }) =>
+      varient === "primary" ? "#86E3C6" : theme.colors.paleGrey};
+    border: solid thin
+      ${({ theme, varient }) =>
+        varient === "primary" ? "#86E3C6" : theme.colors.cloudyBlue};
+    border-radius: 5px;
+  }
+`;
+interface ButtonProps extends React.ButtonHTMLAttributes<any> {
+  varient: "default" | "primary";
+}
+
+const SubmitButton: React.FunctionComponent<ButtonProps> = ({
+  children,
+  ...rest
+}) => {
+  return (
+    <StyledButton {...rest} varient={rest.varient}>
+      {children}
+    </StyledButton>
+  );
+};
+
+export default SubmitButton;
